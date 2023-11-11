892c0363c436c88ae461d14ff4bc3ef71fb12361
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-11-11 21:31:19 +1000

    feat: add styles for header

--- sushi-yum/src/components/Header.tsx
+++ sushi-yum/src/components/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <div className="header">
       <div className="container">
         <div className="header__logo">
-          <img width="38" src={logoSvg} alt="Pizza logo" />
+          <img width="38" src={logoSvg} alt="Shushi logo" />
           <div>
             <h1>React Shushi Yum</h1>
             <p>лучшие суши и роллы в этой вселенной</p>
